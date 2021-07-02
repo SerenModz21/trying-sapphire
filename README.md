a13e8571b54b645772e2da006b631a42cba66c63
Seren_Modz 21 <41450647+SerenModz21@users.noreply.github.com>
2021-07-02 19:47:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ts-template
+
+A template for my TypeScript projects
---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+/*
+ * Copyright © SerenModz21 2018 - 2021 All Rights Reserved.
+ * Unauthorized distribution of any code within this project may result in consequences chosen by the Seren_Modz 21.
+ * Refer to the README for more information.
+ */
+
